76f487642ecb8e3fc55709632a4d9b9dc35abb70
AlphaCentaureye <wzcoffer@gmail.com>
2025-11-03 18:04:05 -0800

    updated readme and added listener socket and sender socket to test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # NetworkScanner
 Project to implement a network scanner through a partial TCP handshake.  
 Using GO language for this project.  
+Creating for Linux Using WSL.  
--- scanner.go
+++ scanner.go
@@ -1,8 +1,7 @@
-
 package main
 
 import (
-	"log",
+	"log"
 	"syscall"
 )
 
@@ -14,21 +13,41 @@ func main() {
 	}
 
 	// bind the socket to an available port
-	err := syscall.Bind(socket, &syscall.SockaddrInet4{
-		Port: 0
-	})
-	if err != nil {
+	if err := syscall.Bind(socket, &syscall.SockaddrInet4{
+		Port: 0,
+	}); err != nil {
 		syscall.Close(socket)
 		log.Panicln("Failed to bind TCP socket: ", err)
 	}
 
 	// retrieve the assigned port number
-	addr, er := systcall.Getsockname(socket)
+	addr, er := syscall.Getsockname(socket)
 	if er != nil {
 		syscall.Close(socket)
 		log.Panicln("Failed to get socket name: ", er)
 	}
+	defer syscall.Close(socket)
 
+	// print the reserved address
+	log.Println(addr)
 
+	// create socket to listen on
+	listen, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
+	if err != nil {
+		syscall.Close(listen)
+		log.Panicln("Failed to create TCP listener socket: ", err)
+	}
+	defer syscall.Close(listen)
+
+	// create raw socket to send packets on
+	send, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	if err != nil {
+		syscall.Close(send)
+		log.Panicln("Failed to create TCP sender socket: ", err)
+	}
+	defer syscall.Close(send)
+
+	log.Println((listen))
+	log.Println((send))
 
 }
